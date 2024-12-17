6a9ad6c1a916028d1bef071e57f31fda8cec9ce8
“olivcat” <184215778+olivcat@users.noreply.github.com>
2024-12-17 14:45:48 +0000

    Ticket 3 complete - Using Get Quotes function and Get request

--- app.js
+++ app.js
@@ -19,3 +19,15 @@ app.get("/", function (req, res) {
 app.listen(PORT, function () {
   console.log(`Server is now listening on http://localhost:${PORT}`);
 });
+
+//Ticket 3
+app.get("/quotes", async function (req, res) {
+  //Write a get request, select correct location to retrieve response.
+  //Respond with the quotes
+  const quotes = await getQuotes(); // Create a variable and store the function call and await promise
+  res.json(quotes); //Return quotes and stringify them from the JSON package.
+});
+
+////Write a request handler to return the correct response
+//perform the correct action when a GET request is received to /quotes.
+//Choose the appropriate helper function from quote.js to get your data.
